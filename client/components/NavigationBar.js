a05dab4c8b0030953a9727ea7ebed0eae5b698b5
louiskane <lkane02@yahoo.com>
2017-08-19 13:52:01 -0700

    adding form

--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link}  from 'react-router';
 
 export default () => {
 
@@ -6,12 +7,12 @@ export default () => {
 		<nav className="navbar navbar-default">
 			<div className="container-fluid">
 				<div className="navbar-header">
-					<a className="navbar-brand" href="#">Red Dice</a>
-				</div>
+					<Link to="/" className="navbar-brand">Red Dice</Link>
+ 				</div>
 
 				<div className="collapse navbar-collapse">
 					<ul className="nav navbar-nav navbar-right">
-						<li><a href="#">Sign up</a></li>
+						<li><Link to="/signup">Sign up</Link></li>
 					</ul>
 				</div>
 			</div>
