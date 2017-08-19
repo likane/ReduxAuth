dbfe31c589c78abcd29da0408270ff9f3661ce29
louiskane <lkane02@yahoo.com>
2017-08-19 14:05:05 -0700

    adding timezones

--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -1,19 +1,90 @@
 import React from 'react';
+import timezones from '../../data/timezones';
 
 class SignupForm extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: '',
+			email: '',
+			password: '',
+			passwordConfirmation: '',
+			timezone: ''
+		}
+
+		this.onChange=this.onChange.bind(this);
+		this.onSubmit=this.onSubmit.bind(this);
+	}
+
+	onChange(e){
+		this.setState({ [e.target.name]: e.target.value } );
+	}
+
+	onSubmit(e) {
+		e.preventDefault();
+		console.log(this.state);
+	}
+
 	render() {
 		return (
-			<form>
+			<form onSubmit={this.onSubmit}>
  				<h1> Join our community</h1>
 
  				<div className="form-group">
  					<label className="control-label">Username</label>
  					<input
+ 						value={this.state.username}
+ 						onChange={this.onChange}
  						type="text"
  						name="Username"
  						className="form-control"
  					/>
  				</div>
+
+ 				<div className="form-group">
+ 					<label className="control-label">email</label>
+ 					<input
+ 						value={this.state.email}
+ 						onChange={this.onChange}
+ 						type="text"
+ 						name="Username"
+ 						className="form-control"
+ 					/>
+ 				</div>
+
+ 				<div className="form-group">
+ 					<label className="control-label">password</label>
+ 					<input
+ 						value={this.state.password}
+ 						onChange={this.onChange}
+ 						type="text"
+ 						name="Username"
+ 						className="form-control"
+ 					/>
+ 				</div>
+
+ 				<div className="form-group">
+ 					<label className="control-label">password confirmation</label>
+ 					<input
+ 						value={this.state.passwordConfirmation}
+ 						onChange={this.onChange}
+ 						type="text"
+ 						name="Username"
+ 						className="form-control"
+ 					/>
+ 				</div>
+
+ 				<div className="form-group">
+ 					<label className="control-label">Username</label>
+ 					<input
+ 						value={this.state.username}
+ 						onChange={this.onChange}
+ 						type="text"
+ 						name="Username"
+ 						className="form-control"
+ 					/>
+ 				</div> 
 
  				<div className="form-group">
  					<button className="btn btn-primary btn-lg">
--- client/components/signup/SignupPage.js
+++ client/components/signup/SignupPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SignupForm from 'SignupForm';
 
+
 class SignupPage extends React.Component {
 	render() {
 		return (
